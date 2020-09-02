f517e0cf1b62b2626deca0989461699c71b301ef
Anakin <sebastian@iparking.com.ar>
2020-09-02 10:37:22 -0300

    Validator-Success

---
+++ backend/src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Moment = require('moment');
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const port = 4000
+const time = Moment(new Date()).format("YYYY:MM:DD-HH:mm:ss");
+
+app.get('/', (req, res) => {
+    console.log(req.body);
+    console.log(time)
+    res.send('Hello World' + ' ' + time);
+})
+
+app.post('/validator', (req, res) => {
+    console.log(req.body);
+    console.log(time);
+    res.send('Validator');
+});
+
+app.post('/response_failure', (req, res) => {
+    console.log(req.body);
+    console.log(time);
+    res.send('Response_Failure');
+})
+
+app.post('/response_success', (req, res) => {
+    console.log(req.body);
+    console.log(time);
+    res.send('Response_Success')
+})
+
+app.post('/transaction_notification', (req, res) => {
+    console.log(req.body);
+    console.log(time);
+    res.send('Transaction_Notification')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ frontend/first-data-validator/src/component/Form.js
@@ -0,0 +1,95 @@
+import React, {useState} from 'react';
+import Moment from "moment";
+import CryptoJS from 'crypto-js';
+
+function Form() {
+    const [data, setData] = useState({
+        txntype: 'sale',
+        timezone: 'America/Buenos_Aires',
+        txndatetime: Moment(new Date()).format("YYYY:MM:DD-HH:mm:ss"),
+        hash_algorithm: 'HMACSHA256',
+        storename: '5920063001',
+        mode: 'payonly',
+        chargetotal: '100',
+        currency: '032',
+        responseFailURL: 'http://8ab3c3801805.ngrok.io/response_failure',
+        responseSuccessURL: 'http://8ab3c3801805.ngrok.io/response_success',
+        transactionNotificationURL: 'http://8ab3c3801805.ngrok.io/transaction_notification',
+        paymentMethod: 'M',
+        cardnumber: '5165 8500 0000 0008',
+        expmonth: '12',
+        expyear: '22',
+        cvm: '123',
+        numberOfInstallments: 1,
+        installmentsInterest: false,
+        transactionId: "1597534682",
+        full_bypass: true,
+        language: "es_ES",
+        oid: '1597534682'
+    });
+
+    function createHash(chargetotal, currency){
+        let FIRSTDATA_SHAREDSECRET='rG5?Jn~6ur';
+        let separator = '|';
+        let stringToExtendedHash =
+            data.cardnumber+separator+
+            chargetotal+separator+
+            currency+separator+
+            data.cvm+separator+
+            data.expmonth+separator+
+            data.expyear+separator+
+            data.full_bypass+separator+
+            data.hash_algorithm+separator+
+            data.installmentsInterest+separator+
+            data.language+separator+
+            data.mode+separator+
+            data.numberOfInstallments+separator+
+            // data.oid+separator+
+            data.paymentMethod+separator+
+            data.responseFailURL+separator+
+            data.responseSuccessURL+separator+
+            data.storename+separator+
+            data.timezone+separator+
+            // data.transactionId+separator+
+            data.transactionNotificationURL+separator+
+            data.txndatetime+separator+
+            data.txntype;
+        let hashHMACSHA256 = CryptoJS.HmacSHA256(stringToExtendedHash, FIRSTDATA_SHAREDSECRET);
+        return CryptoJS.enc.Base64.stringify(hashHMACSHA256);
+    }
+
+    const hashValue = createHash(data.chargetotal, data.currency);
+
+    return(
+        <div>
+            <h1>Formulario</h1>
+            <form method="post" action="https://test.ipg-online.com/connect/gateway/processing">
+                <input className={"form-control"} type="text" name={"txntype"} defaultValue={data.txntype}/>
+                <input className={"form-control"} type="text" name={"timezone"} defaultValue={data.timezone}/>
+                <input className={"form-control"} type="text" name={"txndatetime"} defaultValue={data.txndatetime}/>
+                <input className={"form-control"} type="text" name={"hash_algorithm"} defaultValue={data.hash_algorithm}/>
+                <input className={"form-control"} type="text" name={"storename"} defaultValue={data.storename}/>
+                <input className={"form-control"} type="text" name={"mode"} defaultValue={data.mode}/>
+                <input className={"form-control"} type="text" name={"chargetotal"} defaultValue={data.chargetotal}/>
+                <input className={"form-control"} type="text" name={"currency"} defaultValue={data.currency}/>
+                <input className={"form-control"} type="text" name={"responseFailURL"} defaultValue={data.responseFailURL}/>
+                <input className={"form-control"} type="text" name={"responseSuccessURL"} defaultValue={data.responseSuccessURL}/>
+                <input className={"form-control"} type="text" name={"transactionNotificationURL"} defaultValue={data.transactionNotificationURL}/>
+                <input className={"form-control"} type="text" name={"paymentMethod"} defaultValue={data.paymentMethod}/>
+                <input className={"form-control"} type="text" name={"hashExtended"} defaultValue={hashValue}/>
+                <input className={"form-control"} type="text" name={"cardnumber"} defaultValue={data.cardnumber}/>
+                <input className={"form-control"} type="text" name={"expmonth"} defaultValue={data.expmonth}/>
+                <input className={"form-control"} type="text" name={"expyear"} defaultValue={data.expyear}/>
+                <input className={"form-control"} type="text" name={"cvm"} defaultValue={data.cvm}/>
+                <input className={"form-control"} type="text" name={"full_bypass"} value={data.full_bypass}/>
+                <input className={"form-control"} type="text" name={"language"} value={data.language}/>
+                {/*<input className={"form-control"} type="text" name={"oid"} value={data.oid}/>*/}
+                {/*<input className={"form-control"} type="text" name={"transactionId"} value={data.transactionId}/>*/}
+                <input className={"form-control"} type="text" name={"numberOfInstallments"} value={data.numberOfInstallments}/>
+                <input className={"form-control"} type="text" name={"installmentsInterest"} value={data.installmentsInterest}/>
+                <button className={"btn btn-success"} type="submit" >Enviar</button>
+            </form>
+        </div>
+    );
+}
+export default Form;
